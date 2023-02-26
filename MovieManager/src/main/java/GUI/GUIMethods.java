31310a6b98ae9774c31a4bcdd910eddd0a7185b3
KDesp <kdesp2003@gmail.com>
2023-02-26 17:36:24 +0200

    changed font size

--- MovieManager/src/main/java/GUI/GUIMethods.java
+++ MovieManager/src/main/java/GUI/GUIMethods.java
@@ -52,7 +52,7 @@ public class GUIMethods {
                 }
 
                 try {
-                        GUIMethods.changeGlobalFont(new Component[]{frame}, 0, (String)Database.db().SELECT("Settings", "Font").get(0));
+                        GUIMethods.changeGlobalFont(new Component[]{frame}, 2, (String)Database.db().SELECT("Settings", "Font").get(0));
                 } catch (SQLException ex) {
                         Logger.getLogger(Frame.class.getName()).log(Level.SEVERE, null, ex);
                 }
