5747c14cdf2398a0382933b1cf13c3f0a976d928
creatorkostas <iee2021106@iee.ihu.gr>
2023-03-17 19:13:30 +0200

    FilesList test

--- MovieManager/src/test/java/Files/FilesListTest.java
+++ MovieManager/src/test/java/Files/FilesListTest.java
@@ -1,25 +1,43 @@
 package Files;
 
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FilesListTest {
     @Test
     public void testGetName() {
-
+        /*
+        String dir = System.getProperty("user.dir").replaceAll(Pattern.quote("\\"), "/") +  "/data/";
+        System.out.println(dir);
+        FilesList files = new FilesList(dir);
+        Assert.assertNotNull(files.getName(0));
+         */
     }
 
     @Test
     public void testGetNames() {
-
+        String dir = System.getProperty("user.dir").replaceAll(Pattern.quote("\\"), "/") +  "/data/";
+        FilesList files = new FilesList(dir);
+        Assert.assertNotNull(files.getNames());
     }
 
     @Test
     public void testGetPath() {
-
+        /*
+        String dir = System.getProperty("user.dir").replaceAll(Pattern.quote("\\"), "/") +  "/data/";
+        FilesList files = new FilesList(dir);
+        Assert.assertNotNull(files.getPath(0));
+         */
     }
 
     @Test
     public void testGetPaths() {
+        String dir = System.getProperty("user.dir").replaceAll(Pattern.quote("\\"), "/") +  "/data/";
+        FilesList files = new FilesList(dir);
+        
+        Assert.assertNotNull(files.getPaths());
 
     }
 }
